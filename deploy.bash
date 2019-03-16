c593a5d92e7dc683864a3d8a702a30b92fcb9522
Pavel Katskov <pasha_kackov@mail.ru>
2019-03-16 12:25:08 +0300

    add deploy bash script

--- deploy.bash
+++ deploy.bash
@@ -1,3 +1,8 @@
+git clone https://github.com/ichega/best_hack_legion.git
+cd best_hack_legion
 pip3 install virtualenv
-python3 -m virtualenv venv
+python3 -m venv venv
 source venv/bin/activate
+pip install -r requirements.txt
+cd bh_site
+exec python manage.py runserver 127.0.0.1:8901	
