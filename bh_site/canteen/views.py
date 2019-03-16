c2fde304a217ac5d372d59796468a5e803e8dc4f
Pavel Katskov <pasha_kackov@mail.ru>
2019-03-16 18:29:26 +0300

    add simple menu

--- bh_site/canteen/views.py
+++ bh_site/canteen/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
 from .models import *
 from django.contrib.auth.models import User
 
