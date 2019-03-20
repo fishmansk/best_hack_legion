f225e4e8e56f6fb783e7bbd5e9a0914827ba7031
nikita <bomberman192@yandex.ru>
2019-03-20 16:45:12 +0300

    add comment

--- bh_site/canteen/views.py
+++ bh_site/canteen/views.py
@@ -13,11 +13,18 @@ import json
 
 
 def GET_Manage_Canteens_Id_View(request):
+    '''
+    Получение списка столовых, доступных для редактирования
+    :param request:
+    :return:{
+                "canteens_manage_id": [1],
+            }
+    '''
     if request.user.is_authenticated:
 
-        username = request.user
+        username = request.user#получение идентификатора пользователя
 
-        user = User.objects.get(username=username)
+        user = User.objects.get(username=username)#получение информации о пользователе из базы данных
         canteens = CanteenModel.objects.all()
 
         manage_canteens = []
@@ -35,10 +42,27 @@ def GET_Manage_Canteens_Id_View(request):
 
 
 def GET_Canteens_View(request):
-    #time =
 
+    '''
+    Получение списка всех столовых
+    :param request:
+    :return: {"canteens":[
+                        {"id":1,
+                        "name":"Столовая 1"
+                        "description":"часы работы: 9.00 - 18.00"
+                        },
+                        {"id":2,
+                        "name":"Столовая 2"
+                        "description":"часы работы: 10.00 - 16.00"
+                        },
+                        {"id":3,
+                        "name":"Столовая 3"
+                        "description":"часы работы: 10.00 - 18.00"
+                        }
+                        ]
+            }
+    '''
     canteens = CanteenModel.objects.all()
-    #response{"canteens":[{}]}
     canteen_info = []
     for canteen in canteens:
         canteen_info.append({"id":canteen.pk,
@@ -48,7 +72,47 @@ def GET_Canteens_View(request):
     return JsonResponse(response)
 
 def GET_Categories_View(request):
-    canteen_id=1
+
+    '''
+    Получение списка категорий для столовой с первичным ключом "id"
+    :param request:
+    {
+        "canteen_id":2
+    }
+    :return:
+    {
+    "categories": [
+        {
+            "id": 11,
+            "name": "Первые блюда",
+            "image": "1_5sXeEEP.jpg",
+            "num_dishes": 2
+        },
+        {
+            "id": 12,
+            "name": "Вторые блюда",
+            "image": "2_OwtXqg5.jpg",
+            "num_dishes": 2
+        },
+        {
+            "id": 13,
+            "name": "Напитки",
+            "image": "напики.jpg",
+            "num_dishes": 2
+        },
+        {
+            "id": 14,
+            "name": "Пирожные",
+            "image": "пироженые.jpg",
+            "num_dishes": 2
+        }
+    ]
+    }
+    '''
+
+    data = request.body.decode()
+    data = json.loads(data)
+    canteen_id = data["canteen_id"]
     #canteen_id = request.category_id
     categories = CategoryModel.objects.filter(canteen=canteen_id)
     category_info = []
@@ -63,12 +127,25 @@ def GET_Categories_View(request):
     return JsonResponse(response)
 
 def POST_Category_View(request):
-    Add_Category = {
+    '''
+    Добавление категории
+    :param request:
+     {
         "name":"гарнир",
         "canteen":1,
-        "image":'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAYAAAD+Bd/7AAAABHNCSVQICAgIfAhkiAAAADZJREFUCJlj/P///38GPICFgYGBgZGREavk////IQpgRvhCGVvUEYYy4TMeYQWMh8GAWYHHnQAAhA802d3H5gAAAABJRU5ErkJggg==',
-        "image_name": "keklol.png",
+        "image":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAYAAAD+Bd/7AAAABHNCSVQICAgIfAhkiAAAADZJREFUCJlj/P///38GPICFgYGBgZGREavk////IQpgRvhCGVvUEYYy4TMeYQWMh8GAWYHHnQAAhA802d3H5gAAAABJRU5ErkJggg==",
+        "image_name":"гарнир.png"
     }
+    :return:
+    {
+        "name": "гарнир",
+        "canteen": 1,
+        "image": "18f21f37-6589-414e-8aef-6294a71a02a7_гарнир.png",
+        "image_name": "гарнир.png",
+        "id": 49
+    }
+    '''
+
     data = request.body.decode()
     data = json.loads(data)
     Add_Category = data
@@ -90,13 +167,25 @@ def POST_Category_View(request):
     return JsonResponse(Add_Category)
 
 def PATCH_Category_View(request):
-    Add_Category = {
-        "id":"44",
-        "name":"PPPPPPPPPPPPPPPPPPPPPPPPPPPPPP",
+    '''
+    Редактирование категории с первичным ключом "id"
+    :param request:
+     {
+        "id":"49"
+        "name":"Гарнир",
         "canteen":1,
-        "image":'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAYAAAD+Bd/7AAAABHNCSVQICAgIfAhkiAAAADZJREFUCJlj/P///38GPICFgYGBgZGREavk////IQpgRvhCGVvUEYYy4TMeYQWMh8GAWYHHnQAAhA802d3H5gAAAABJRU5ErkJggg==',
-        "image_name":"keklllke.png",
+        "image":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAYAAAD+Bd/7AAAABHNCSVQICAgIfAhkiAAAADZJREFUCJlj/P///38GPICFgYGBgZGREavk////IQpgRvhCGVvUEYYy4TMeYQWMh8GAWYHHnQAAhA802d3H5gAAAABJRU5ErkJggg==",
+        "image_name":"гарнир.png"
     }
+    :return:
+   {
+        "id": "49",
+        "name": "Гарнир",
+        "canteen": 1,
+        "image": "1474b146-decf-4511-bd20-d682315bcc2c_гарнир.png",
+        "image_name": "гарнир.png"
+    }
+    '''
 
     data = request.body.decode()
     data = json.loads(data)
@@ -118,12 +207,24 @@ def PATCH_Category_View(request):
 
     category.save()
 
-    Add_Category['image']=category.image
+    Add_Category['image']=str(category.image)
     return JsonResponse(Add_Category)
 
 
 def DELETE_Category_View(request):
 
+
+    '''
+    Удаление категории с первичным ключом "id"
+    :param request:
+    {
+        "id":49
+    }
+    :return:
+    {
+        "all":"right"
+    }
+    '''
     data = request.body.decode()
     data = json.loads(data)
     Delete_Category = data
@@ -134,7 +235,45 @@ def DELETE_Category_View(request):
 
 
 def GET_Dishes_View(request):
-    category_id=5
+    '''
+    Получение информации о блюдах в категории "category_id"
+    :param request:
+    {
+        "category_id":14
+    }
+    :return:
+    {
+    "dishes": [
+        {
+            "id": 18,
+            "name": "Корзиночка с маком",
+            "calories": 90,
+            "proteins": 30,
+            "fats": 20,
+            "carbohydrates": 50,
+            "price": 40,
+            "mass": 100,
+            "description": "Самая вкусная корзиночка с маком.",
+            "image": "корзиночка_с_маком.jpg"
+        },
+        {
+            "id": 19,
+            "name": "Бисквитное пироженое",
+            "calories": 150,
+            "proteins": 20,
+            "fats": 20,
+            "carbohydrates": 60,
+            "price": 40,
+            "mass": 100,
+            "description": "Самое вкусное бисквитное пирожное в этой столовой",
+            "image": "бисквитное_пирожное.jpg"
+        }
+    ]
+}
+    '''
+    data = request.body.decode()
+    data = json.loads(data)
+    category_id = data["category_id"]
     dishes = DishModel.objects.filter(category=category_id)
     dishes_info = []
     print(dishes)
@@ -155,25 +294,48 @@ def GET_Dishes_View(request):
 
 
 def POST_Dish_View(request):
-    Add_Dish = {
-        "name":"dadadaddff",
-        "category":20,
-        "image":'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAYAAAD+Bd/7AAAABHNCSVQICAgIfAhkiAAAADZJREFUCJlj/P///38GPICFgYGBgZGREavk////IQpgRvhCGVvUEYYy4TMeYQWMh8GAWYHHnQAAhA802d3H5gAAAABJRU5ErkJggg==',
-        "proteins":20,
-        "calories":20,
-        "fats":20,
-        "carbohydrates":20,
-        "price":20,
-        "mass":20,
-        "description":"dadada",
-        "image_name": "ada.png"
-    }
 
+
+
+    '''
+    Добавление блюда
+    :param request:
+    {
+            "name": "пюре",
+            "calories": 150,
+            "category":48,
+            "proteins": 35,
+            "fats": 35,
+            "carbohydrates": 30,
+            "price": 30,
+            "mass": 200,
+            "description": "добавление блюда пюре",
+            "image":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAYAAAD+Bd/7AAAABHNCSVQICAgIfAhkiAAAADZJREFUCJlj/P///38GPICFgYGBgZGREavk////IQpgRvhCGVvUEYYy4TMeYQWMh8GAWYHHnQAAhA802d3H5gAAAABJRU5ErkJggg==",
+            "image_name":"пюре.png"
+    }
+    :return:
+    {
+            "name": "пюре",
+            "category": 48,
+            "calories": 150,
+            "proteins": 35,
+            "fats": 35,
+            "carbohydrates": 30,
+            "price": 30,
+            "mass": 200,
+            "description": "добавление блюда пюре",
+            "image": "db90662e-564d-47db-833a-d033f90464e8_пюре.png",
+            "image_name": "пюре.png",
+            "id": 33
+    }
+    '''
 
 
     data = request.body.decode()
     data = json.loads(data)
     Add_Dish = data
+
+
 
     image = Add_Dish["image"].split(',')[1]
     image_64_decode = base64.b64decode(image)
@@ -204,20 +366,40 @@ def POST_Dish_View(request):
 
 
 def PATCH_Dish_View(request):
-    Add_Dish = {
-        "id":29,
-        "name":"dishhhhhh",
-        "category":20,
-        "image":'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAYAAAD+Bd/7AAAABHNCSVQICAgIfAhkiAAAADZJREFUCJlj/P///38GPICFgYGBgZGREavk////IQpgRvhCGVvUEYYy4TMeYQWMh8GAWYHHnQAAhA802d3H5gAAAABJRU5ErkJggg==',
-        "proteins":20,
-        "calories":20,
-        "fats":20,
-        "carbohydrates":20,
-        "price":20,
-        "mass":20,
-        "description":"dadada",
-        "image_name": "ada.png"
+
+    '''
+    Редактирование блюда с превичным ключом "id"
+    :param request:
+    {
+            "id":33,
+            "name": "Пюре",
+            "calories": 150,
+            "category":48,
+            "proteins": 35,
+            "fats": 35,
+            "carbohydrates": 30,
+            "price": 30,
+            "mass": 200,
+            "description": "добавление блюда пюре",
+            "image":"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAgAAAAGCAYAAAD+Bd/7AAAABHNCSVQICAgIfAhkiAAAADZJREFUCJlj/P///38GPICFgYGBgZGREavk////IQpgRvhCGVvUEYYy4TMeYQWMh8GAWYHHnQAAhA802d3H5gAAAABJRU5ErkJggg==",
+            "image_name":"пюре.png"     
+        }
+    :return:
+    {
+            "id": 33,
+            "name": "Пюре",
+            "category": 48,
+            "calories": 150,
+            "proteins": 35,
+            "fats": 35,
+            "carbohydrates": 30,
+            "price": 30,
+            "mass": 200,
+            "description": "добавление блюда пюре",
+            "image": "db90662e-564d-47db-833a-d033f90464e8_пюре.png",
+            "image_name": "пюре.png"
     }
+    '''
 
     data = request.body.decode()
     data = json.loads(data)
@@ -228,8 +410,6 @@ def PATCH_Dish_View(request):
     media_root = settings.MEDIA_ROOT
     filename = str(uuid.uuid4())+str("_")+ str(Add_Dish["image_name"])
     path = os.path.join(media_root, filename)
-    print(path)
-    # image_result = open('media/category_'+str(category.)+'.png', 'wb')
     file = open(path, 'wb')
     file.write(image_64_decode)
     Dish = DishModel(pk=Add_Dish['id'])
@@ -247,17 +427,29 @@ def PATCH_Dish_View(request):
     Dish.description = Add_Dish['description']
 
 
-    # image_result.write(image_64_decode)
     Dish.save()
     Add_Dish['image']=str(Dish.image)
     return JsonResponse(Add_Dish)
 
 
 def DELETE_Dish_View(request):
+
+
+    '''
+    Удаление блюда с первичным ключом "id"
+    :param request:
+    {
+        "id":33
+    }
+    :return:
+    {
+    "all": "right"
+    }
+    '''
+
     data = request.body.decode()
     data = json.loads(data)
     Delete_Dish = data
-    #Delete_Dish={'id': 29}
     DishModel.objects.filter(id=Delete_Dish['id']).delete()
     return JsonResponse({"all":"right"})
 
